9b93246888a3b75482ba4baf48d71d66d3291ffd
Serhii Holub <serhii.holub@UA02273.local>
2023-03-22 15:52:24 +0200

    Instance Types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-let isPresent: boolean = false;
-let magic: number = 66.6;
-let hello: string = 'world';
+let regExp: RegExp = new RegExp('ab+c');
+let array: Array<number> = [1, 2, 3];
+let set: Set<number> = new Set([1, 2, 3]);
 
-let notDefined: undefined = undefined;
-let notPresent: null = null;
+/** A first in a first out collection */
 
-let penta: symbol = Symbol('star');
-let biggy: bigint = 24n;
+class Queue<T> {
+    private data: Array<T> = [];
+    push(item: T) { this.data.push(item); }
+    pop(): T | undefined { return this.data.shift(); }
+}
+
+let queue: Queue<number> = new Queue();
